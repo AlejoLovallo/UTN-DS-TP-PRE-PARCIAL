6e5cc8078d68a6d2931566c8acd238143b3d1f9d
Alejo Lovallo <alejoe.lovallo@gmail.com>
2022-08-10 11:23:31 -0300

    pre diseño, para tirar

---
+++ src/main/java/Domain/BuscadorDeInformacion/Endpoints/Golf/Basquet/Api.java
@@ -0,0 +1,4 @@
+package Domain.BuscadorDeInformacion.Endpoints.Golf.Basquet;
+
+public interface Api {
+}
--- src/main/java/Domain/Deportes/Deporte.java
+++ src/main/java/Domain/Deportes/Deporte.java
@@ -3,4 +3,5 @@ package Domain.Deportes;
 public interface Deporte {
   // QUIZAS USAR STRATEGY DENTRO DEL PARAMETRO
   void procesarInformacion();
+  void generarEstadisticas();
 }
